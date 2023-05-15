93129e7e01262677d4cd33a76d8ffec7f0cf9b68
Ronan Lenouvel <ronan.develop@gmail.com>
2023-05-15 15:27:58 +0200

    (exercice 3): ✔ done

--- index.php
+++ index.php
@@ -17,3 +17,38 @@ sort($color);
 foreach ($color as $y){
     echo $y . ", ";
 }
+/**
+ * Exercice 3
+ */
+$ceu = [
+    "Italy"=>"Rome",
+    "Luxembourg"=>"Luxembourg",
+    "Belgium"=>"Brussels",
+    "Denmark"=>"Copenhagen",
+    "Finland"=>"Helsinki",
+    "France" => "Paris",
+    "Slovakia"=>"Bratislava",
+    "Slovenia"=>"Ljubljana",
+    "Germany" => "Berlin",
+    "Greece" => "Athens",
+    "Ireland"=>"Dublin",
+    "Netherlands"=>"Amsterdam",
+    "Portugal"=>"Lisbon",
+    "Spain"=>"Madrid",
+    "Sweden"=>"Stockholm",
+    "United Kingdom"=>"London",
+    "Cyprus"=>"Nicosia",
+    "Lithuania"=>"Vilnius",
+    "Czech Republic"=>"Prague",
+    "Estonia"=>"Tallin",
+    "Hungary"=>"Budapest",
+    "Latvia"=>"Riga",
+    "Malta"=>"Valetta",
+    "Austria" => "Vienna",
+    "Poland"=>"Warsaw"
+];
+asort($ceu);
+foreach ($ceu as $k => $c){
+    echo "The capital of " . $k . " is " . $c . "\n";
+}
+// sort array by value
